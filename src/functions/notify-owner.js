d6c5689583e7e4a12e3b4ecc58bc8b041d6e9331
Ru Singh <inbox@ruppalsingh.com>
2020-05-16 21:37:27 +0530

    Log event and context details

--- src/functions/notify-owner.js
+++ src/functions/notify-owner.js
@@ -2,26 +2,33 @@
 // Send an email to the site owner with comment details, approve link, and a discard/delete link
 
 exports.handler = (event, context, callback) => {
-	const sgMail = require("@sendgrid/mail");
-	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+	// const sgMail = require("@sendgrid/mail");
+	// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 	const ownerEmail = "inbox@rusingh.com";
 	const siteName = "Open JAMStack Comments";
 
-	const msg = {
-		to: ownerEmail,
-		from: ownerEmail,
-		subject: `New comment received on ${siteName}`,
-		text: `
-			You have received a new comment, details as follows:
-			Event: ${event}
-			Context: ${context}
-			`,
-		html: `
-			You have received a new comment, details as follows:
-			<strong>Event:</strong> <pre>${event}</pre>
-			<strong>Context:</strong> <pre>${context}</pre>
-			`
-	};
-	sgMail.send(msg);
+	console.log(event, context);
+
+	callback(null, {
+		statusCode: 200,
+		body: "Done running"
+	});
+
+	// const msg = {
+	// 	to: ownerEmail,
+	// 	from: ownerEmail,
+	// 	subject: `New comment received on ${siteName}`,
+	// 	text: `
+	// 		You have received a new comment, details as follows:
+	// 		Event: ${event}
+	// 		Context: ${context}
+	// 		`,
+	// 	html: `
+	// 		<p>You have received a new comment, details as follows:</p>
+	// 		<p><strong>Event:</strong> <pre>${event}</pre></p>
+	// 		<p><strong>Context:</strong> <pre>${context}</pre></p>
+	// 		`
+	// };
+	// sgMail.send(msg);
 };
