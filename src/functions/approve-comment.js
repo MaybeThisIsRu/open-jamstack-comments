b66bd2ce91724d70bc3b32dc8827fc28785d37f7
Ru Singh <inbox@ruppalsingh.com>
2020-05-24 13:35:29 +0530

    Log the event in approve-comment

--- src/functions/approve-comment.js
+++ src/functions/approve-comment.js
@@ -1,4 +1,5 @@
 exports.handler = (event, context, callback) => {
-	const params = JSON.parse(event.queryStringParameters);
-	console.log("Params for approve-comment: ", params);
+	console.log(event);
+	// const params = JSON.parse(event.queryStringParameters);
+	// console.log("Params for approve-comment: ", params);
 };
